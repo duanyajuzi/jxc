c3f9482a346c708d71be48820877808888d0fd4b
LZGH <1183071782@qq.com>
2018-03-30 13:12:03 +0800

    订单项修改

---
+++ src/main/webapp/pages/orderinfo/js/inStock.js
@@ -0,0 +1,83 @@
+
+var PageInoutStock=function () {
+    return {
+        defaultOption: {
+            basePath: "",
+            inoutItemGrid:null,
+        },
+        init: function () {
+            mini.parse();
+            this.basePath = PageMain.basePath;
+            this.inoutItemGrid=mini.get("inoutItemGrid");
+            this.inoutItemGrid.load();
+        },
+
+        funSearch: function () {
+            var businessId= mini.get("businessId").getValue().trim();
+            var orderNo = mini.get("orderNo").getValue().trim();
+            var orderName=mini.get("orderName").getValue().trim();
+            var materialNum=mini.get("materialNum").getValue().trim();
+            var stimeBegin=mini.get("stimeBegin");
+            var stimeEnd=mini.get("stimeEnd");
+            var paramData={"businessId":businessId,"orderNo":orderNo,"orderName":orderName,"materialNum":materialNum,"stimeBegin":stimeBegin.text,"stimeEnd":stimeEnd.text};
+            this.inoutItemGrid.load(paramData, function(res){
+                console.log(res);
+            });
+        },
+        funReset:function () {
+            var inoutStockForm=new mini.Form("inoutStockForm");
+            inoutStockForm.setData();
+            this.inoutItemGrid.load();
+        },
+        funModify : function()
+        {
+            var row = this.inoutItemGrid.getSelected();
+            if(row)
+            {
+                row.ycmaterialNum=row.materialNum;
+                var paramData = {action: "updateInoutStockItem", row: row, title:"修改价格"};
+                this.funOpenInfo(paramData);
+            }
+            else
+            {
+                PageMain.funShowMessageBox("请选择一条记录");
+            }
+        },
+        funOpenInfo : function(paramData)
+        {
+            var me = this;
+            mini.open({
+                url: this.basePath + "/pages/orderinfo/inoutStockItem_add.jsp",
+                title: paramData.title,
+                width: 400,
+                height: 280,
+                onload:function(){
+                    var iframe=this.getIFrameEl();
+                    iframe.contentWindow.PageInoutStockItemAdd.funSetData(paramData);
+                },
+                ondestroy:function(action){
+                    me.inoutItemGrid.reload();
+                }
+            })
+        },
+        //商品入库数量
+        funSetInoutStockNum: function () {
+           mini.open({
+               url:this.basePath+"/pages/orderinfo/setInStock.jsp",
+               height:600,
+               width:1100,
+               onload:function () {
+                   var iframe=this.getIFrameEl();
+                   iframe.contentWindow.PageSetInoutStock.funGetData();
+               },
+               ondestroy:function () {
+                   mini.get("inoutItemGrid").load();
+               }
+           });
+        }
+    }
+} ();
+
+$(function(){
+    PageInoutStock.init();
+});
